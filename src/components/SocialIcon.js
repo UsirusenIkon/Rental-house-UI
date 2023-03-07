06c8c4e0ee641a15b993e004e4e708b162412eb9
Ahmad zaki Yousufi <ahmadzakiyousufi3@gmail.com>
2023-03-08 02:38:57 +0430

    Add social icons to sidebar

---
+++ src/components/SocialIcon.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  TiSocialTwitter, TiSocialFacebook, TiSocialGooglePlus, TiSocialVimeo, TiSocialPinterest,
+} from 'react-icons/ti';
+
+const SocialIcon = () => (
+  <div className="social-logo">
+    <a className="social-link" href="google.com" target="_blank"><TiSocialTwitter size={25} /></a>
+    <a className="social-link" href="google.com" target="_blank"><TiSocialFacebook size={25} /></a>
+    <a className="social-link" href="google.com" target="_blank"><TiSocialGooglePlus size={25} /></a>
+    <a className="social-link" href="google.com" target="_blank"><TiSocialVimeo size={25} /></a>
+    <a className="social-link" href="google.com" target="_blank"><TiSocialPinterest size={25} /></a>
+  </div>
+
+);
+
+export default SocialIcon;
