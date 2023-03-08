726eb78ed83a823ccd73ea0ac6030f5dda471757
Usirusen Ikon <lexicon048@gmail.com>
2023-03-08 18:33:07 +0100

    Fix eslint errors

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,13 +1,16 @@
-import { useState } from 'react';
-import { React } from 'react';
+import { useState, React } from 'react';
+
 import { NavLink } from 'react-router-dom';
-import './sidebar.css'
-import { BsHouseDoorFill, BsFillHouseAddFill, BsBookmarkStarFill, BsBookmarkCheckFill, BsFillBookmarkPlusFill, BsFillHouseDashFill } from 'react-icons/bs';
+import './sidebar.css';
+import {
+  BsHouseDoorFill, BsFillHouseAddFill, BsBookmarkStarFill,
+  BsBookmarkCheckFill, BsFillBookmarkPlusFill, BsFillHouseDashFill,
+} from 'react-icons/bs';
 import { GoSignOut } from 'react-icons/go';
-import mainLogo from '../../assests/App-logo.svg'
+import { FiMenu } from 'react-icons/fi';
+import { GrClose } from 'react-icons/gr';
+import mainLogo from '../../assests/logo-dark.png';
 import SocialIcon from '../SocialIcon';
-import { FiMenu } from 'react-icons/fi'
-import { GrClose } from 'react-icons/gr'
 
 function Sidebar() {
   const width = window.innerWidth < 768;
@@ -18,51 +21,67 @@ function Sidebar() {
   };
 
   return (
-<div>
-  <button 
-    onClick={onClickHandler}
-    className={`hamburger_menu`}>
-    {open ? <GrClose /> : <FiMenu />}
-  </button>
-  <div className={`sidebar ${open ? 'open' : 'close'}`}>
-    <div className="logo-box">
-      <img className="nav-logo" src={mainLogo} alt="App Logo" />
-    </div>
-    <div className="nav-links">
-        <NavLink to="/" className={width ? 'mobile-nav-link' : 'nav-box'} exact>
-          HOUSE LIST <BsHouseDoorFill/>
-        </NavLink>
-    
-        <NavLink to="/add-house" className={width ? 'mobile-nav-link' : 'nav-box'}>
-          ADD HOUSE <BsFillHouseAddFill/>
-        </NavLink>
-    
-        <NavLink to="/delete-house" className={width ? 'mobile-nav-link' : 'nav-box'}>
-        DELETE HOUSE <BsFillHouseDashFill/>  
-        </NavLink>
-        
-        <NavLink to="/reservelist" className={width ? 'mobile-nav-link' : 'nav-box'}>
-         RESERVE LIST <BsBookmarkStarFill/>
-        </NavLink>
-        
-        <NavLink to="/my-reservations" className={width ? 'mobile-nav-link' : 'nav-box'}>
-          MY RESERVATIONS <BsBookmarkCheckFill/>
-        </NavLink>
-    
-        <NavLink to="/reserves" className={width ? 'mobile-nav-link' : 'nav-box'}>
-          ADD RESERVATION <BsFillBookmarkPlusFill/>
-        </NavLink>
-        
-        <NavLink to="/logout" className={width ? 'mobile-nav-link logout' : 'nav-box logout'}>
-        Log out  <GoSignOut/>
-        </NavLink>
+    <div>
+      <button
+        type="button"
+        onClick={onClickHandler}
+        className="hamburger_menu"
+      >
+        {open ? <GrClose /> : <FiMenu />}
+      </button>
+      <div className={`sidebar ${open ? 'open' : 'close'}`}>
+        <div className="logo-box">
+          <img className="nav-logo" src={mainLogo} alt="App Logo" />
         </div>
-    
-    <div className="social-box">
-        <SocialIcon/>
+        <div className="nav-links">
+          <NavLink to="/" className={width ? 'mobile-nav-link' : 'nav-box'} exact>
+            HOUSE LIST
+            {' '}
+            <BsHouseDoorFill />
+          </NavLink>
+
+          <NavLink to="/add-house" className={width ? 'mobile-nav-link' : 'nav-box'}>
+            ADD HOUSE
+            {' '}
+            <BsFillHouseAddFill />
+          </NavLink>
+
+          <NavLink to="/delete-house" className={width ? 'mobile-nav-link' : 'nav-box'}>
+            DELETE HOUSE
+            {' '}
+            <BsFillHouseDashFill />
+          </NavLink>
+
+          <NavLink to="/reservelist" className={width ? 'mobile-nav-link' : 'nav-box'}>
+            RESERVE LIST
+            {' '}
+            <BsBookmarkStarFill />
+          </NavLink>
+
+          <NavLink to="/my-reservations" className={width ? 'mobile-nav-link' : 'nav-box'}>
+            MY RESERVATIONS
+            {' '}
+            <BsBookmarkCheckFill />
+          </NavLink>
+
+          <NavLink to="/reserves" className={width ? 'mobile-nav-link' : 'nav-box'}>
+            ADD RESERVATION
+            {' '}
+            <BsFillBookmarkPlusFill />
+          </NavLink>
+
+          <NavLink to="/logout" className={width ? 'mobile-nav-link logout' : 'nav-box logout'}>
+            Log out
+            {' '}
+            <GoSignOut />
+          </NavLink>
+        </div>
+
+        <div className="social-box">
+          <SocialIcon />
+        </div>
+      </div>
     </div>
-  </div>
-</div>
   );
-};
+}
 export default Sidebar;
