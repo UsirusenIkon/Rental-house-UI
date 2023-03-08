30f8557d6ff3cf8fff111f0da629114a13983ab2
Ahmad zaki Yousufi <ahmadzakiyousufi3@gmail.com>
2023-03-08 19:51:18 +0430

    Add save method user info save to local storage

--- src/api/config.js
+++ src/api/config.js
@@ -1,6 +1,15 @@
-const API_URL = 'http://localhost:3000/api/v1/';
+const API_URL = 'http://127.0.0.1:3000/';
 
+// saving user_name in the browser's local storage for future use
+const USERNAME = localStorage.getItem('user_name') || 'guest';
+const isLogged = localStorage.getItem('user_name') || false;
+const login = (username) => {
+  localStorage.setItem('user_name', username);
+};
+const logout = () => {
+  localStorage.removeItem('user_name');
+  window.location = '/login';
+};
 export {
-    API_URL
-  };
-  
+  API_URL, USERNAME, isLogged, login, logout,
+};
